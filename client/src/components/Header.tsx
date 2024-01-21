865f08153da18cc856ed9b3c744ddf1b163a283c
Aviroop Jana <aviroopjana@gmail.com>
2024-01-22 02:49:35 +0530

    Refactor header component and add dynamic navigation links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,12 +7,16 @@ import { Link, useLocation } from "react-router-dom";
 const Header = () => {
   const path = useLocation().pathname;
 
+  const navLinks = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/projects", label: "Projects" },
+  ];
+
   return (
     <Navbar className="border-b-2">
-      <Link
-        to={"/"}
-        className="self-center whitespace-nowrap flex items-center text-2xl sm:text-3xl font-bold dark:text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-500 focus:ring-pink-500 focus:ring-opacity-50 transition-transform hover:scale-105"
-      >
+        
+      <Link to={"/"} className={headerLinkStyles}>
         <span className="px-2 py-1">Tech</span>
         <SiGooglelens className="mt-3 pr-2" size={40} />
       </Link>
@@ -25,9 +29,11 @@ const Header = () => {
           className="hidden md:inline"
         />
       </form>
+
       <Button className="w-8 md:w-12 h-8 md:h-10 md:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
+
       <div className="flex gap-2 md:gap-2 lg:gap-3 md:order-2">
         <Button
           className="w-8 md:w-12 h-8 md:h-10 sm:inline hidden "
@@ -41,19 +47,22 @@ const Header = () => {
         </Link>
         <Navbar.Toggle />
       </div>
+
       <Navbar.Collapse>
-        <Navbar.Link as={"div"} active={path === "/"}>
-          <Link to="/">Home</Link>
-        </Navbar.Link>
-        <Navbar.Link as={"div"} active={path === "/about"}>
-          <Link to={"/about"}>About</Link>
-        </Navbar.Link>
-        <Navbar.Link as={"div"} active={path === "/projects"}>
-          <Link to={"/projects"}>Projects</Link>
-        </Navbar.Link>
+        {navLinks.map((navLink) => (
+            <Navbar.Link key={navLink.label} as={"div"} active={path === navLink.path}>
+                <Link to={navLink.path}>
+                    {navLink.label}
+                </Link>
+            </Navbar.Link>
+        ))}
       </Navbar.Collapse>
+
     </Navbar>
   );
 };
 
 export default Header;
+
+const headerLinkStyles =
+  "self-center whitespace-nowrap flex items-center text-2xl sm:text-3xl font-bold dark:text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-500 focus:ring-pink-500 focus:ring-opacity-50 transition-transform hover:scale-105";
