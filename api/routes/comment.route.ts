9b5cb378f684e1f257da00aca8a292c39d45c2e9
Aviroop Jana <aviroopjana@gmail.com>
2024-02-22 20:41:33 +0530

    feat: Implement API endpoint to fetch comments for a specific post

--- api/routes/comment.route.ts
+++ api/routes/comment.route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser';
-import { createComment } from '../controllers/comment.controller';
+import { createComment, getPostComments } from '../controllers/comment.controller';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
+router.get('/getPostComments/:postId', getPostComments);
 
 export default router;
