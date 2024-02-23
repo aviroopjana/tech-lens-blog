857d4b20cf4e8a7e74082291ae8403bfecd73fd1
aviroopjana <aviroopjana@gmail.com>
2024-02-24 04:36:09 +0530

    Implement api for editing comments

--- api/routes/comment.route.ts
+++ api/routes/comment.route.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser';
-import { createComment, getPostComments, likeComment } from '../controllers/comment.controller';
+import { createComment, editComment, getPostComments, likeComment } from '../controllers/comment.controller';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 
 export default router;
