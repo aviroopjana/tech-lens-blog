d3ea75d77a5f2ce8b847d635af181420c0ded995
aviroopjana <aviroopjana@gmail.com>
2024-02-24 06:10:20 +0530

    Implement api to delete comment

--- api/routes/comment.route.ts
+++ api/routes/comment.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser';
-import { createComment, editComment, getPostComments, likeComment } from '../controllers/comment.controller';
+import { createComment, deleteComment, editComment, getPostComments, likeComment } from '../controllers/comment.controller';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.put('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;
