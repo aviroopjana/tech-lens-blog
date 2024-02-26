5cf1641ac18ebf2c8c2c4f77366d5722179b779f
aviroopjana <aviroopjana@gmail.com>
2024-02-26 23:33:54 +0530

    Check if proxy is working

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api' : 'https://tech-lens-blog-server.vercel.app',
+      "/api": {
+        target: "https://tech-lens-blog-server.vercel.app",
+        changeOrigin:true
+      },
     },
   },
   plugins: [react()],
-})
+});
